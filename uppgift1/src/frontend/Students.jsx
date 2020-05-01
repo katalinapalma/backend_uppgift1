e8fb588435da84dee80df0aea623fde172c3a92c
katalinapalma <kata_92@hotmail.com>
2020-05-01 21:30:47 +0200

    fixed table and added a form to create a student

--- uppgift1/src/frontend/Students.jsx
+++ uppgift1/src/frontend/Students.jsx
@@ -2,33 +2,78 @@ import React, { Component, Fragment } from 'react';
 import styles from '../frontend/Students.module.css';
 
 class Students extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: [
+        {name: 'Django', id: 1, age: 20},
+        {name: 'Dr. King', id: 2, age: 60},
+        {name: 'Calvin Candie', id: 3, age: 30},
+        {name: 'Broomhilda', id: 4, age: 31},
+      ],
+    }
+  }
+
+  deleteStudent = (e) => {
+    console.log('deleted student')
+  }
+
+  createStudent = (e) => {
+    console.log('created student')
+  }
+
+  renderTableData() {
+    return this.state.students.map((student, index) => {
+       const { id, name, age} = student //destructuring
+       return (
+          <tr key={id}>
+             <td>{name}</td>
+             <td>{id}</td>
+             <td>{age}</td>
+          </tr>
+       )
+    })
+ }
+
+ renderTableHeader() {
+  let header = Object.keys(this.state.students[0])
+  return header.map((key, index) => {
+     return <th className={styles.tHeader} key={index}>{key.toUpperCase()}</th>
+  })
+}
+
   render() {
     return (
       <Fragment>
-        <div className={styles.tableContainer}>
-          <table className={styles.table}>
-            <tr className={styles.tableTR}>
-              <th className={styles.tableTitle}>Students</th>
-              <th className={styles.tableTitle}>Contact</th>
-              <th className={styles.tableTitle}>Country</th>
-            </tr>
-            <tr>
-              <td className={styles.tableTitle}>Alfreds Futterkiste</td>
-              <td className={styles.tableTitle}>Maria Anders</td>
-              <td className={styles.tableTitle}>Germany</td>
-            </tr>
-            <tr>
-              <td className={styles.tableTitle}>Centro comercial Moctezuma</td>
-              <td className={styles.tableTitle}>Francisco Chang</td>
-              <td className={styles.tableTitle}>Mexico</td>
-            </tr>
-            <tr>
-              <td className={styles.tableTitle}>Aaliyah</td>
-              <td className={styles.tableTitle}>Aaliyh@gmail.com</td>
-              <td className={styles.tableTitle}>Switzerland</td>
-            </tr>
+        <h2 className={styles.studentsH2}>Students</h2>
+        <div className={styles.container}>
+          <table className={styles.students}>
+             <tbody>
+             <tr>
+               {this.renderTableHeader()}
+              </tr>
+              {this.renderTableData()}
+             </tbody>
           </table>
-        </div>
+       </div>
+
+       <h3 className={styles.studentsH3}>Want to create a student?</h3>
+       <div className={styles.container}>
+         <form className={styles.form}>
+            <label htmlFor="name" className={styles.formTitles}>Name:</label>
+            <input type="text" className="form-control" placeholder='name'></input>
+            <label htmlFor="email" className={styles.formTitles}>Email:</label>
+            <input type="email" className="form-control" placeholder='email'></input>
+            <label htmlFor="address" className={styles.formTitles}>Address:</label>
+            <input type="text" className="form-control" placeholder='address'></input>
+            <button 
+              type="button" 
+              className="btn btn-dark mt-3"
+              onClick={this.createStudent}>
+                Create
+            </button>
+         </form>
+       </div>
       </Fragment>
     )
   }
@@ -36,3 +81,49 @@ class Students extends Component {
 
 export default Students;
 
+// <div className={styles.tableContainer}>
+//           <table className={styles.table}>
+//             <tbody>
+//             <tr className={styles.tableTR}>
+//               <th className={styles.tableTitle}>Name</th>
+//               <th className={styles.tableTitle}>Email</th>
+//               <th className={styles.tableTitle}></th>
+//             </tr>
+//             </tbody>
+//             <tbody>
+//             <tr>
+//               <td className={styles.tableTitle}>Alfreds Futterkiste</td>
+//               <td className={styles.tableTitle}>Maria Anders</td>
+//               <button 
+//                 className={styles.deleteBtn} 
+//                 onClick={this.deleteStudent}
+//                 >
+//                 delete
+//               </button>
+//             </tr>
+//             </tbody>
+//             <tbody>
+//             <tr>
+//               <td className={styles.tableTitle}>Centro comercial Moctezuma</td>
+//               <td className={styles.tableTitle}>Francisco Chang</td>
+//                      <button 
+//                 className={styles.deleteBtn} 
+//                 onClick={this.deleteStudent}
+//                 >
+//                 delete
+//               </button>
+//             </tr>
+//             </tbody>
+//             <tbody>
+//             <tr>
+//               <td className={styles.tableTitle}>Aaliyah</td>
+//               <td className={styles.tableTitle}>Aaliyah@gmail.com</td>
+//                      <button 
+//                 className={styles.deleteBtn} 
+//                 onClick={this.deleteStudent}
+//                 >
+//                 delete
+//               </button>
+//             </tr>
+//             </tbody>
+//           </table>
