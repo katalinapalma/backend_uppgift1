17bb6a01bc6112121850d1c51d271b75d2057066
katalinapalma <kata_92@hotmail.com>
2020-05-02 21:03:37 +0200

    added backend things

--- uppgift1/src/backend/index.js
+++ uppgift1/src/backend/index.js
@@ -1,12 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+// const router = express.Router();
 
 // environment variable PORT or 3000 if unset
-// change by executing "export PORT=2000" in bash before starting
-const port = process.env.PORT || 3000;
+// change by executing 'export PORT=2000' in bash before starting
+// const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send({"Hello": "World"});
+app.get('/api', (req, res) => {
+  res.send({'Hello': 'World'});
+});
+
+app.get('/api/test', (req, res) => {
+  res.send([1, 2, 3, 4]);
 });
 
 // Start up server and begin listen to requests
